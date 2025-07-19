9bfb50c134a34acab1a1ded40257794d49733bcf
HarshlyHk <harsh4212002@gmail.com>
2025-07-19 23:54:41 +0530

    Added Dashboard page and extended App router for routed view support

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard"; // ⬅️ New import
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Home />} />
+                <Route path="/dashboard" element={<Dashboard />} />{" "}
+                {/* ⬅️ New route */}
             </Routes>
         </BrowserRouter>
     );
